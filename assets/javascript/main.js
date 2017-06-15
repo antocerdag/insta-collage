47ef97b19d6557ea67d89795c534aa07557e217f
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-15 19:27:49 -0400

    validacion lista

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,16 @@
-main.js
+var botoncito = document.getElementById("botoncito");
+
+botoncito.addEventListener("click", validacion);
+
+
+function validacion(){
+	var username = document.getElementById("name").value;
+	var psw = document.getElementById("psw").value;
+
+	if(username == ""){
+		alert("Por favor ingresa tu nombre de usuario o correo");
+	}
+	if(psw == 123456 || psw == "" && (!(/^\d{6}$/.test(psw))) ){
+		alert("Por favor ingresa una clave correcta");
+	}
+}	
